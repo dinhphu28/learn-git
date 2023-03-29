60453e677fe1ae2c9a6b9145194a8e94014424d7
Anthony <18110175@student.hcmute.edu.vn>
2023-03-29 22:02:23 +0700

    Update main.java: Update function sum - u2

---
+++ main.java
@@ -0,0 +1,15 @@
+class Program {
+	public static void main(String[] args) {
+		System.out.println("Started");
+
+		int a = 1, b = 2;
+
+		int result = sum(a,b);
+
+		System.out.println("Sum of " + a + " and " + b + " is: " + result);
+	}
+
+	private static int sum(int a, int b) {
+		return (a + b);
+	}
+}
